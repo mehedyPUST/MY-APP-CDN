af4a7eb1c9249516cf544dcc1efc2721a80c60f1
mehedyPUST <mehedy.pust@gmail.com>
2026-03-14 13:20:48 +0600

    2nd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this if deploying to GitHub Pages
+  base: '/MY-APP-CDN/', // Only needed for GitHub Pages
 })
